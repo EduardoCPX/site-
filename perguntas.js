3525f782c6d4ac16c2e3639b1c25ece1db3b9d2a
EduardoCPX <teleken.peres@escola.pr.gov.br>
2025-11-13 13:58:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,38 @@
+criaCartao(
+    'Educação Financeira',
+    'O que é educação financeira?',
+    'É o conjunto de conhecimentos e práticas que ajudam uma pessoa a administrar melhor o seu dinheiro — entender como ganhar, gastar, investir e economizar de forma consciente.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que significa “gastar menos do que se ganha”?',
+    'É a base da saúde financeira. Significa que suas despesas devem ser menores que sua renda, para que sobre dinheiro para poupar ou investir.'
+)
+
+criaCartao(
+    'Educação Financeira',
+    'Qual é a diferença entre poupar e investir?',
+    'Poupar é guardar dinheiro, geralmente em conta ou poupança Investir é aplicar o dinheiro em algo que pode gerar rendimento (como ações, CDB, Tesouro Direto, etc.)'
+   
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que são despesas fixas e variáveis?',
+    'Fixas: contas que não mudam muito de valor (aluguel, internet, transporte). Variáveis: mudam de acordo com o mês (lazer, alimentação fora de casa, compras)'
+
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é o “juros composto”?',
+    'É o famoso “juros sobre juros”. O rendimento de um investimento é somado ao valor inicial e passa a render novamente.    Com o tempo, faz o dinheiro crescer de forma acelerada.'
+
+)
+
+criaCartao(
+    'Educação Financeira',
+    'O que é um fundo de investimento?',
+    'É uma forma de aplicar dinheiro em grupo. Várias pessoas investem juntas, e um gestor profissional decide onde aplicar (ações, renda fixa, câmbio, etc.). Cada investidor tem uma parte do fundo, chamada de cota.'
+)
